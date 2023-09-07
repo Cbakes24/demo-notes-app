6db251237ac6ec6dbb7b5561a2724ae19f010022
Cory Baker <corybaker24@gmail.com>
2023-09-07 12:52:44 -0400

    first commit

---
+++ packages/functions/src/lambda.ts
@@ -0,0 +1,14 @@
+import { ApiHandler } from "sst/node/api";
+
+export const handler = ApiHandler(async (_evt) => {
+  return {
+    statusCode: 200,
+    body: `Hello world. The time is ${new Date().toISOString()}`,
+  };
+});
+
+
+// export const testFunction = () => {
+//   const name = "cory"
+//   return name
+// }
