a0abbbf10e08cc82c8082543eb318cc924997cbc
Cory Baker <corybaker24@gmail.com>
2023-09-11 14:39:34 -0400

    AUthStack file created

---
+++ stacks/AuthStack.ts
@@ -0,0 +1,40 @@
+import { ApiStack } from "./ApiStack";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { StorageStack } from "./StorageStack";
+import { Cognito, StackContext, use } from "sst/constructs";
+
+export function AuthStack({ stack, app }: StackContext) {
+  const { api } = use(ApiStack);
+  const { bucket } = use(StorageStack);
+
+  // Create a Cognito User Pool and Identity Pool
+  const auth = new Cognito(stack, "Auth", {
+    login: ["email"],
+  });
+
+  auth.attachPermissionsForAuthUsers(stack, [
+    // Allow access to the API
+    api,
+    // Policy granting access to a specific folder in the bucket
+    new iam.PolicyStatement({
+      actions: ["s3:*"],
+      effect: iam.Effect.ALLOW,
+      resources: [
+        bucket.bucketArn + "/private/${cognito-identity.amazonaws.com:sub}/*", // a user has access to only their folder within the bucket. This allows us to separate access to our user’s file uploads within the same S3 bucket.
+      ],
+    }),
+  ]);
+
+  // Show the auth resources in the output, (API Endpoint)
+  stack.addOutputs({
+    Region: app.region,
+    UserPoolId: auth.userPoolId,
+    UserPoolClientId: auth.userPoolClientId,
+    IdentityPoolId: auth.cognitoIdentityPoolId,
+  });
+
+  // Return the auth resource
+  return {
+    auth,
+  };
+}
