5feb27dabe9dd66156e6a407ec6a5af621ffab9b
Cory Baker <corybaker24@gmail.com>
2023-09-11 16:14:38 -0400

    Adding a billing API with Stripe

---
+++ packages/functions/src/billing.ts
@@ -0,0 +1,26 @@
+import Stripe from "stripe";
+import { Config } from "sst/node/config";
+import handler from "@notes/core/handler";
+import { calculateCost } from "@notes/core/cost";
+
+export const main = handler(async (event) => {
+    //storage is number of notes user wnats to store
+    //source is the Stripe token for the card that will be charged
+  const { storage, source } = JSON.parse(event.body || "{}"); 
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
+
+  // Load our secret key
+  const stripe = new Stripe(Config.STRIPE_SECRET_KEY, {
+    apiVersion: "2023-08-16",
+  });
+
+  await stripe.charges.create({
+    source,
+    amount,
+    description,
+    currency: "usd",
+  });
+
+  return JSON.stringify({ status: true });
+});
